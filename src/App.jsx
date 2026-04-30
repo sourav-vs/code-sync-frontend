554646e7ab73872f4b2b8f7d6ee0e704da61db0f
sourav11223454 <souravvs2255@gmail.com>
2026-04-30 21:06:52 +0530

    codeSync phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import workSpace from './pages/workSpace'
+
 
 function App() {
 
   return (
    <>
-   <h1>app</h1>
+   <Routes>
+    <Route path='/'><Home /></Route>
+    <Route path='/workSpace'><workSpace /></Route>
+   </Routes>
    </>
   )
 }
---
+++ src/pages/WorkSpace.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function workSpace() {
+  return (
+    <div>workSpace</div>
+  )
+}
+
+export default workSpace
